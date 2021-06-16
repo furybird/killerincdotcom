b38031cbfeb0227be6deeba5dc50de619bb381a8
Jennifer Williams <jenw20@gmail.com>
2021-06-16 11:33:37 -0400

    Resync HTML folder

---
+++ php/contactthanks.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Thanks for reaching out to Killer inc.</title>
+	<meta name="description" content="Killer inc. We make things for people who make things.">
+	<meta name="keywords" content="design, consulting, nyc, brooklyn, frog design, sticker ui book, user experience, ui kit, ui patterns, user research, product definition, product design, software, service design, mobile, tablet, web, beacon phone booth">
+	<meta name="author" content="Killer inc.">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="../stylesheets/base.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="../stylesheets/skeleton.css" type="text/css" charset="utf-8">
+	<link rel="stylesheet" href="../stylesheets/layout.css" type="text/css" charset="utf-8">
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<!-- Web Fonts
+	================================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="icon" type="image/png" href="../images/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="../images/favicon-16x16.png" sizes="16x16" />
+	
+</head>
+
+<body style="background:#231f20;">
+
+	<div id="main">
+		
+		<!-- THANKS TEXT -->
+		<div class="text thanks">			
+			<div class="container">
+
+				<div class="three columns">&nbsp;</div>
+				
+				<div class="ten columns"> 						
+					<a name="contact"><h2>Your Message Has Been Sent!</h2></a>
+					<p>Thanks for reaching out. We'll be sure to get back to you swiftly. </br>In the meantime, take a gander at our current projects, </br><a href="http://stickerui.com" target="_blank">The Sticker UI Book</a> & <a href="http://stickerui.com/digitalkit" target="_blank">the digital Sticker UI Kit</a></p>
+					
+					<object type="image/svg+xml" data="../svg/killerinc_logo-bomb.svg">
+							Your browser does not support SVG
+					</object>
+				</div>
+						
+				<div class="three columns">&nbsp;</div>
+				
+			</div><!-- end container -->
+		</div><!-- end text thanks-->	
+	
+	</div><!-- end main-->	
+</body>
+
+</html>
